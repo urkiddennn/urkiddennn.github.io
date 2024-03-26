d8a8279e536f225af76370a2e0a25efed09fbfa4
urkiddennn <154726035+urkiddennn@users.noreply.github.com>
2024-03-26 14:18:16 +0800

    por

---
+++ poryou/main.js
@@ -0,0 +1,19 @@
+var inputField = document.getElementById("inputTogo");
+const message = document.getElementById("message");
+const help = document.getElementById("help");
+
+var messagePage = "message";
+var back = "back";
+inputField.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        if (inputField.value == messagePage) {
+            help.style.display = "none";
+            message.style.display = "flex";
+            console.log(inputField.value);
+        } else if (inputField.value == back) {
+            help.style.display = "block";
+            message.style.display = "none";
+            console.log("back");
+        }
+    }
+});
